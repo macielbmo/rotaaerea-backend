60c2a47ff61ca57de13b9479f9bdb90e9393d84c
Maciel <macielbmo@gmail.com>
2023-12-21 19:07:38 -0300

    ..

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,6 +1,5 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'http://localhost:5173');
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Access-Control-Max-Age', '10');
