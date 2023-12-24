dad5bab88a6da13ac66c334c4a08a4029b06269d
Maciel <macielbmo@gmail.com>
2023-12-24 16:30:55 -0300

    cofig multer para o deploy

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,66 +1,25 @@
-const multer = require('multer');
-const path = require('path');
-const crypto = require('crypto');
-
 const { S3Client } = require('@aws-sdk/client-s3');
+const multerS3 = require('multer-s3');
+const crypto = require('crypto');
+const multer = require('multer');
 
 const client = new S3Client({ region: 'sa-east-1' });
-const multerS3 = require('multer-s3');
 
-const storageTypes = {
-  // local: multer.diskStorage({
-  //   destination: (req, file, cb) => {
-  //     cb(null, path.resolve(__dirname, '..', '..', 'temp', 'uploads'));
-  //   },
+const storage = multerS3({
+  s3: client,
+  bucket: 'photo-rotaaerea',
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  acl: 'public-read',
+  key: (req, file, cb) => {
+    crypto.randomBytes(16, (err, hash) => {
+      if (err) cb(err);
 
-  //   filename: (req, file, cb) => {
-  //     crypto.randomBytes(16, (err, hash) => {
-  //       if (err) cb(err);
-
-  //       file.key = `${hash.toString('hex')}-${file.originalname}`;
-
-  //       cb(null, file.key);
-  //     });
-  //   },
-  // }),
-
-  s3: multerS3({
-    s3: client,
-    bucket: 'photo-rotaaerea',
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: 'public-read',
-    key: (req, file, cb) => {
-      crypto.randomBytes(16, (err, hash) => {
-        if (err) cb(err);
-
-        const fileName = `${hash.toString('hex')}-${file.originalname}`;
-
-        cb(null, fileName);
-      });
-    },
-  }),
-};
-
-module.exports = {
-  dest: path.resolve(__dirname, '..', '..', 'temp', 'uploads'),
-
-  storage: storageTypes[process.env.STORAGE_TYPE],
-  limits: {
-    fileSize: 2 * 1024 * 1024,
+      const fileName = `${hash.toString('hex')}-${file.originalname}`;
+      cb(null, fileName);
+    });
   },
-  fileFilter: (req, file, cb) => {
-    const allowedMimes = [
-      'image/jpeg',
-      'image/jpg',
-      'image/pjpeg',
-      'image/png',
-      'image/gif',
-    ];
+});
 
-    if (allowedMimes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Invalid file type'));
-    }
-  },
-};
+const upload = multer({ storage });
+
+module.exports = upload;
