62308b256d7dbbda32e980e6c9bf31663c2a5440
Maciel <macielbmo@gmail.com>
2024-01-07 08:59:44 -0300

    Atualização da politica de CORS

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,7 +1,9 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '10');
+  if (response) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '10');
+  }
   next();
 };
